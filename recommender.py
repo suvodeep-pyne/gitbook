203e23a1e1fdfaf99a9cbf245ade94bbe54a3121
garima87 <garima@ubuntu.(none)>
2013-04-29 23:36:31 -0500

    Changed recommender

--- recommender.py
+++ recommender.py
@@ -7,15 +7,16 @@ Edited by Garima Agarwal
 
 import operator
 import pickle
-import pprint as pp
 import pickle
+import cmd
+import pprint as pp
 
 from page_rank import pagerank
 from collections import defaultdict
 from read_data import DataRetriever
 from nb_classifier import NaiveBayesClassifier
 
-GITHUB_DATA = 'github_data'
+GITHUB_DATA = 'data_ash'
 TRAIN_DATA = 'train_data'
 
 class ProjectVectorBuilder():
@@ -32,20 +33,23 @@ class ProjectVectorBuilder():
     def build_projects_vector(self):
         print "In build projects"
         for name, project in self.project_data.iteritems():
-            self.projects[name] = {}
-            #readme = project['readme']
+            readme = project['readme']
             
             # Bad case: When readme is not found. It returns empty lists.
-            #if isinstance(readme, list): continue
-            #readme = unicode(readme, 'utf-8', errors = 'ignore')
-            if project["description"] != None:
-                readme = project["description"]
+            if isinstance(readme, list): 
+                readme = ""
             else:
-                continue
+                readme = unicode(readme, 'utf-8', errors = 'ignore')
 
+            if project['description'] != None:
+                readme += project['description']
+            
+            if readme == "":    continue
+
+            self.projects[name] = {}
             prob_data = self.nb.classify(readme)[0]
             self.projects[name]['class_prob'] = prob_data
-            self.projects[name]['description'] = readme
+            self.projects[name]['description'] = project['description']
             if len(prob_data) > 0:
                 self.projects[name]['category'] = max(prob_data.iteritems(), key=operator.itemgetter(1))[0]
         return self.projects
@@ -55,10 +59,14 @@ class Recommender():
     """Initialize the recommender"""
     def __init__(self):
         directory_name = GITHUB_DATA
+        print "Reading data..........."
         self.data_retriever = DataRetriever(directory_name)
+        print "Done reading data"
         self.project_data = self.data_retriever.parseProjectData()
         self.user_data, self.user_follower_map = self.data_retriever.parseUserFollowers()
+        print "Training Naive's Bayes classifier"
         self.project_vector_builder = ProjectVectorBuilder(self.project_data)
+        print "Trained!!!"
         self.language_proj = defaultdict()   
 
     def get_languages(self):
@@ -75,7 +83,7 @@ class Recommender():
     """Get different scores for each project"""
     def build_project_features(self):
         self.project_vector = self.project_vector_builder.build_projects_vector()
-        self.user_ranking = pagerank(self.user_data, self.user_follower_map)
+        #self.user_ranking = pagerank(self.user_data, self.user_follower_map)
         with open('lang_to_projects.p') as f:
           self.language_proj = pickle.load(f)
         
@@ -83,24 +91,54 @@ class Recommender():
           self.difficulty_score = pickle.load(f)
                  
     def recommend_projects(self, languages, area_interest, difficulty): 
+        print "Calling recommender"
         projects = set()
+        print languages
+        print area_interest
+        print difficulty
         for language in languages:
             projects = projects.union(self.language_proj[language]) 
         
         similar_projects = []
         for project in projects:
+            if project not in self.project_vector:   continue
+
             if self.project_vector[project]['category'] in area_interest:
                 similar_projects.append(self.project_vector[project])
             
         pp.pprint(similar_projects)
 
+class CommandLineInterface(cmd.Cmd):
+    
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.obj = Recommender()
+        self.obj.build_project_features()
+        self.name = "garima"
+        self.prompt = ">> "
+    
+    def do_recommend_projects(self, args):
+        languages, aoi, level = args.split("\" \"")
+        languages = languages.replace("\"", "")
+        level = level.replace("\"", "")
+        self.obj.recommend_projects(languages.split(), [aoi], level)
+    
+    def do_EOF(self, args):
+        return self.do_exit(args)
+ 
+    def do_exit(self, args):
+        """Exit"""
+        return -1  
+    
+    def do_help(self, args):
+        cmd.Cmd.do_help(self, args)
 
 if __name__ == '__main__':
-    obj = Recommender()
-    obj.build_project_features()
-    print obj.get_languages()
-    print obj.get_aoi()
-    #obj.recommend_projects(['JavaScript'], ['Web'], ['Hard'])
+    cmi = CommandLineInterface()
+    cmi.cmdloop()
+    #print obj.get_languages()
+    #print obj.get_aoi()
+    print "Getting recommended projects"
     print 'Printing projects Data Structure'
     #pp.pprint(obj.projects)
     #pp.pprint(obj.project_vector)
